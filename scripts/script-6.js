26ef59cce3a7138eef09977fc3f3ec28d4df5152
sjoedwards <samuel.j.o.edwards@gmail.com>
2022-06-27 09:08:00 +0100

    added other scripts

---
+++ scripts/script-6.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 50,
+  duration: '10s',
+  thresholds: {
+    'checks{succeed:true}': ['rate>0.9'],
+  },
+};
+
+export default function () {
+  let res;
+
+  res = http.get('http://httpbin.test.k6.io');
+  check(res, {
+    'status is 500': (r) => r.status == 500,
+  });
+
+  res = http.get('http://httpbin.test.k6.io');
+  check(
+    res,
+    {
+      'status is 200': (r) => r.status == 200,
+    },
+    { succeed: 'true' }
+  );
+
+  sleep(1);
+}
---
+++ scripts/script-7.js
@@ -0,0 +1,9 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+import { group } from 'k6';
+
+export default function () {
+  // GROUP SCRIPT
+  // START HERE
+}
